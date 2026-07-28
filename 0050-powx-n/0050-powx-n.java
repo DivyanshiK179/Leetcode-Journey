487eaf681f7f44da8c57bb9837243fd8c7cf2d7d
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-07-28 23:47:34 +0530

    Time: 0 ms (100%), Space: 48.1 MB (47.48%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N=n;
+        if(N<0)
+        {
+            x=1/x;
+            N=-N;
+        }
+        double pow=1;
+        while(N>0)
+        {
+            if(N%2==1)
+            {
+                pow*=x;
+            }
+            x*=x;
+            N/=2;
+        }
+        return pow;
+    }
+}

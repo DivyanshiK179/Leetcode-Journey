ef73d262ab39350ffad3087056ab0bbf45e19e1d
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-01 13:55:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(2,n+1):
             for j in range(n-i+1):
                 k=j+i-1
-                dp[j]=max(nums[j]-dp[j+ 1],nums[k]-dp[j])
+                dp[j]=max(nums[j]-dp[j+1],nums[k]-dp[j])
         return dp[0]>=0

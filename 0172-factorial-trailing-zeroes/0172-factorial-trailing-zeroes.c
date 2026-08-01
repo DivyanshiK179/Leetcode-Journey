7dc859e2da324331445d1b2bb1a18e651f4c9971
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-01 15:56:11 +0530

    Time: 9 ms (0.69%), Space: 8.7 MB (96.07%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.c
@@ -0,0 +1,15 @@
+int trailingZeroes(int n) {
+        int count=0;
+        unsigned long long fact=1;
+        for(int i=2;i<=n;i++)
+        {
+            fact*=i;
+            while(fact%10==0)
+            {
+                count++;
+                fact/=10;
+            }
+            fact%=10000000000ULL;
+        }
+        return count;
+}

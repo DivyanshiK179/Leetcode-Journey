7781612f9402d7d875d684d217e7de240192b03d
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-09 19:03:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (23.45%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long left=0;
+        long long right=sqrt(c);
+        while(left<=right)
+        {
+            long long cur_sum=left*left+right*right;
+            if(cur_sum==c)
+            {
+                return true;
+            }
+            else if(cur_sum<c)
+            {
+                left+=1;
+            }
+            else
+            {
+                right-=1;
+            }
+        }
+        return false;
+    }
+};

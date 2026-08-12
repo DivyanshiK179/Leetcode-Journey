5a6343cce1667b14b0a5b2b529faad9f4cb04cfb
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-12 10:43:28 +0530

    Time: 0 ms (100%), Space: 9.2 MB (88.37%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,24 @@
+int mySqrt(int x) {
+        long long s=1;
+        long long e=x;
+        long long ans=0;
+        while(s<=e)
+        {
+            long long mid=(s+e)/2;
+            long long sq=mid*mid;
+            if(sq==x)
+            {
+                return mid;
+            }
+            else if(sq<x)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+}

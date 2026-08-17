5a3c3da17e8b538dd481695b0beb8c5ddf5538e6
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-17 10:47:41 +0530

    Time: 19 ms (9.09%), Space: 11.8 MB (9.66%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.c
@@ -0,0 +1,48 @@
+    bool ispossible(int* weights,int weightsSize,int mid,int days)
+    {
+        int day=1;
+        int ship_load=0;
+        for(int i=0;i<weightsSize;)
+        {
+            if(ship_load+weights[i]<=mid)
+            {
+                ship_load+=weights[i];
+                i++;
+            }
+            else
+            {
+                day++;
+                ship_load=0;
+            }
+            if(day>days)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+int shipWithinDays(int* weights, int weightsSize, int days) {
+        int ans=0;
+        int total=0;
+        for(int i=0;i<weightsSize;i++)
+        {
+            total+=weights[i];
+        }
+        int s=1;
+        int e=total;
+        while(s<=e)
+        {
+            int mid=(s+e)/2;
+            if(ispossible(weights,weightsSize,mid,days))
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        return ans;
+}

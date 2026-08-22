5b6b1d8ffb7c37dde9cc5da1b10251104c3e3571
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-22 21:02:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (26.17%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.c
@@ -0,0 +1,19 @@
+bool checkDivisibility(int n) {
+        int temp=n;
+        int sum=0;
+        int prod=1;
+        int total=0;
+        while(temp>0)
+        {
+            int dig=temp%10;
+            sum+=dig;
+            prod*=dig;
+            temp/=10;
+        }
+        total=sum+prod;
+        if(n%total==0)
+        {
+            return true;
+        }
+        return false;
+}

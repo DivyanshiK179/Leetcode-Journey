0a9f8db0e91462688f5d3bf0ccf6c3f3d1cf658c
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-08-24 22:36:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.23%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,41 @@
+bool search(int* nums, int numsSize, int target) {
+        int s=0;
+        int n=numsSize;
+        int e=n-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[s]==nums[mid] && nums[mid]==nums[e])
+            {
+                s++;
+                e--;
+            }
+            else if(nums[mid]>=nums[s])
+            {
+                if(target>=nums[s] && target<nums[mid])
+                {
+                    e=mid-1;
+                }
+                else
+                {
+                    s=mid+1;
+                }
+            }
+            else
+            {
+                if(target<=nums[e] && target>nums[mid])
+                {
+                    s=mid+1;
+                }
+                else
+                {
+                    e=mid-1;
+                }
+            }
+        }
+        return false;
+}

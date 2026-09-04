50291efc02b1c869500c1636dfc9fe903bbe14b7
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-09-04 18:01:45 +0530

    Time: 4 ms (12.5%), Space: 10.1 MB (18.75%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.c
@@ -0,0 +1,26 @@
+int firstStableIndex(int* nums, int numsSize, int k) {
+        for(int i=0;i<numsSize;i++)
+        {
+            int max=nums[0];
+            for(int j=0;j<=i;j++)
+            {
+                if(nums[j]>max)
+                {
+                    max=nums[j];
+                }
+            }
+            int min=nums[i];
+            for(int k=i;k<numsSize;k++)
+            {
+                if(nums[k]<min)
+                {
+                    min=nums[k];
+                }
+            }
+            if((max-min)<=k)
+            {
+                return i;
+            }
+        }
+        return -1;
+}

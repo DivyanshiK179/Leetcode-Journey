2c25797fe3f6302912acb72e157258e2eb3e1551
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-09-07 23:24:32 +0530

    Time: 4 ms (7.53%), Space: 12.7 MB (20.48%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    private:
+    void solve(vector<int> &nums, vector<int> &temp, vector<vector<int>> &output, int i)
+    {
+        if(i==nums.size())
+        {
+            output.push_back(temp);
+            return;
+        }
+        solve(nums,temp,output,i+1);
+        temp.push_back(nums[i]);
+        solve(nums,temp,output,i+1);   
+        temp.pop_back();    
+    }
+
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> output;
+        vector<int> temp;
+        solve(nums,temp,output,0);
+        return output;
+    }
+};

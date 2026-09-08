a817434e8d66c27a79157a4c830b7565457d310e
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-09-08 21:11:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<1000)
+        {
+            return 0;
+        }
+        int commas=0;
+        for(int i=1000;i<=n;i++)
+        {
+            commas+=1;
+        }
+        return commas;
+    }
+};

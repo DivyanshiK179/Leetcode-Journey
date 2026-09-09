ec7ec422917692e05513329d42e2f5551208576c
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-09-09 21:04:42 +0530

    Time: 1 ms (99.36%), Space: 42.5 MB (71.15%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n<4)
+        {
+            return 0;
+        }
+        long commas=0;
+        for(long i=1000;i<=n;i*=1000)
+        {
+            commas+=n-i+1;
+        }
+        return commas;
+    }
+}

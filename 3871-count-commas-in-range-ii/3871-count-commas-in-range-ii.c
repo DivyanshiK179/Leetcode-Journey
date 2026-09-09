7ea79409fba2e4a20fa810b2d63ff50941b1af20
Divyanshi Kesharwani <dkak.77765@gmail.com>
2026-09-09 21:05:20 +0530

    Time: 0 ms (100%), Space: 9.4 MB (57.89%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.c
@@ -0,0 +1,12 @@
+long long countCommas(long long n) {
+        if(n<4)
+        {
+            return 0;
+        }
+        long long commas=0;
+        for(long long i=1000;i<=n;i*=1000)
+        {
+            commas+=n-i+1;
+        }
+        return commas;
+}
